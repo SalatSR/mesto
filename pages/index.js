7a85c14b125bcec4f36b6d74f0e62238930e5b28
Stas Razborskii <stasaktivniy@yandex.ru>
2022-04-03 11:42:27 +0300

    Made 'close' button for popups

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-// Находим окно popup в DOM
-let popupFormContaner = document.querySelector('.popup');
+// Находим окно popup
+let popupWindow = document.querySelector('.popup');
+// Находим нужный popup
+let popupProfile = document.querySelector('.popup__type_profile');
+let popupCard = document.querySelector('.popup__type_card');
+let popupImageView = document.querySelector('.popup__type_image-view');
+// Находим поле Профиля
+let profileElement = document.querySelector('.profile');
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__container');
+// Находим кнопки
+let editBtn = profileElement.querySelector('.profile__edit-button'); // "Edit"
+let addBtn = profileElement.querySelector('.profile__add-button'); // "Add"
+// кнопки закрытия
+let popupCloserProfile = popupProfile.querySelector('.popup__closer_profile'); // popup Profile
+let popupCloserCard = popupCard.querySelector('.popup__closer_card'); // popup Card
+let popupCloserImageView = popupImageView.querySelector('.popup__closer_image-view'); // popup Image
+// кнопки отправки форм
+let popupSubmitProfile = popupProfile.querySelector('.popup__submit-button_profile'); 
+let popupSubmitCard = popupProfile.querySelector('.popup__submit-button_card'); 
 // Находим поля формы в DOM
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
-// Находим кнопку "Close"(popup-profile)
-let popupCloser = formElement.querySelector('.popup__closer');
-
-
-
-// Находим поле Профиля в DOM
-let profileElement = document.querySelector('.profile');
-// Находим кнопку "Edit"(profile) в DOM
-let editBtn = profileElement.querySelector('.profile__edit-button');
 // Находим кнопку ПОля ввода в профиле
 let nameInputProfile = profileElement.querySelector('.profile__info-name');
 let jobInputProfile = profileElement.querySelector('.profile__info-job');
-
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -41,16 +47,40 @@ function formSubmitHandler (evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
+// Открываем Popup
+function openPopup(type) {
+  type.classList.add('popup_opened');
+}
+
+// Закрываем Popup
+function closePopup(type) {
+  type.classList.remove('popup_opened');
+}
+
 // Открываем Popup (profile)
 function openProfile() {
-  popupFormContaner.classList.add('popup_opened');
+  openPopup(popupProfile)
   nameInput.value = nameInputProfile.textContent;
   jobInput.value = jobInputProfile.textContent;
 }
-// Закрываем Popup (profile)
-function closeProfile() {
-  popupFormContaner.classList.remove('popup_opened');
+
+// Открываем Popup (card)
+function openCard() {
+  openPopup(popupCard)
+  nameInput.value = nameInputProfile.textContent;
+  jobInput.value = jobInputProfile.textContent;
 }
-// Слушатели на кнопки "Edit"(profile), "Close"(popup-profile) 
+
+// Открываем Popup (card)
+function openImageView() {
+  openPopup(popupImageView)
+  nameInput.value = nameInputProfile.textContent;
+  jobInput.value = jobInputProfile.textContent;
+}
+
+// Слушатели кнопок
 editBtn.addEventListener('click', openProfile);
-popupCloser.addEventListener('click', closeProfile);
+addBtn.addEventListener('click', openCard);
+popupCloserProfile.addEventListener('click', () => closePopup(popupProfile)); // "Close" (popup Profile)
+popupCloserCard.addEventListener('click', () => closePopup(popupCard)); // "Close" (popup Card)
+popupCloserImageView.addEventListener('click', () => closePopup(popupImageView)); // "Close" (popup Image)
